e189972b497dfca9bd8edb1404f6300077f702a4
Fabian Böttcher <me@cakasim.de>
2020-01-11 10:29:44 +0100

    Add bundle class

---
+++ src/CakasimPayoneBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cakasim\Symfony\PayoneBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Fabian Böttcher <me@cakasim.de>
+ */
+class CakasimPayoneBundle extends Bundle
+{
+}
