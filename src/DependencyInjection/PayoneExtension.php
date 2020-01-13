80c23b210e7a1b94873d93cc5c327ab36f0cf24c
Fabian Böttcher <me@cakasim.de>
2020-01-13 10:12:12 +0100

    Load bundle service config

--- src/DependencyInjection/PayoneExtension.php
+++ src/DependencyInjection/PayoneExtension.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Cakasim\Symfony\Bundle\PayoneBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * @author Fabian Böttcher <me@cakasim.de>
@@ -17,5 +19,7 @@ class PayoneExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
